36ba7b7ceb1e97efc1befff4631b59f4e155cca5
Marvin T <contact@marvinkturner.com>
2021-08-15 01:27:13 -0400

    ✨ feat: add routes for other operations
    this commit will add routes for subtract, multiply, and divide. When supplied with appropriate data, these routes will perform the namesake operation and return the result. Some of the common gotchas are accounted for.

--- app.py
+++ app.py
@@ -10,10 +10,18 @@ api = Api(app)
 
 # HELPER FUNCTIONS
 def check_posted_data(posted_data, function_name):
-    if (function_name == 'add'):
+    if (function_name in {'add', 'sub', 'mtpy'}):
         # check errors
         if "x" not in posted_data or "y" not in posted_data:
             return 301
+        else:
+            return 200
+    elif (function_name == 'div'):
+        if "x" not in posted_data or "y" not in posted_data:
+            return 301
+        # Cannot divide by 0, must validate
+        elif int(posted_data["y"]) == 0:
+            return 302
         else:
             return 200
 
@@ -53,13 +61,105 @@ class Add(Resource):
 
 
 class Subtract(Resource):
-    pass
+    def post(self):
+        # What happens if SUBTRACT was request using POST method
+        # Step 1: Get posted data
+        posted_data = request.get_json()
+
+        # Step : Validate the data
+        status_code = check_posted_data(posted_data, 'sub')
+        # If not OK
+        if (status_code != 200):
+            # give insightful error message
+            res_json = {
+                "Message": "An error occured",
+                "Status Code": status_code
+            }
+            # Send message back
+            return jsonify(res_json)
+
+        
+        # Step 2: Convert data to usable format (int)
+        # status_code == '200' past this point
+        x = int(posted_data["x"])
+        y = int(posted_data["y"])
+
+        # Step 3: Operate on data
+        res = x - y
+
+        # Step 4: Return result
+        res_obj = {
+            'Message': res,
+            'Status Code': 200
+        }
+        return jsonify(res_obj)
 
 class Multiply(Resource):
-    pass
+    def post(self):
+        # What happens if Add was request using POST method
+        # Step 1: Get posted data
+        posted_data = request.get_json()
+
+        # Step : Validate the data
+        status_code = check_posted_data(posted_data, 'mtpy')
+        if (status_code != 200):
+            # give insightful error message
+            res_json = {
+                "Message": "An error occured",
+                "Status Code": status_code
+            }
+            # Send message back
+            return jsonify(res_json)
+
+        
+        # Step 2: Convert data to usable format (int)
+        # status_code == '200' past this point
+        x = int(posted_data["x"])
+        y = int(posted_data["y"])
+
+        # Step 3: Operate on data
+        res = x * y
+
+        # Step 4: Return result
+        res_obj = {
+            'Message': res,
+            'Status Code': 200
+        }
+        return jsonify(res_obj)
 
 class Divide(Resource):
-    pass
+    def post(self):
+        # What happens if Add was request using POST method
+        # Step 1: Get posted data
+        posted_data = request.get_json()
+
+        # Step : Validate the data
+        status_code = check_posted_data(posted_data, 'div')
+        if (status_code != 200):
+            # give insightful error message
+            res_json = {
+                "Message": "An error occured",
+                "Status Code": status_code
+            }
+            # Send message back
+            return jsonify(res_json)
+
+        
+        # Step 2: Convert data to usable format (int)
+        # status_code == '200' past this point
+        x = int(posted_data["x"])
+        y = int(posted_data["y"])
+
+        # Step 3: Operate on data
+        # Mulitply by 1.0 to ensure float
+        res = (x * 1.0) / y
+
+        # Step 4: Return result
+        res_obj = {
+            'Message': res,
+            'Status Code': 200
+        }
+        return jsonify(res_obj)
 
 # ROUTES
 
@@ -72,6 +172,24 @@ def hello_world():
 api.add_resource(
     Add,
     "/add"
+)
+
+# Use api to expose Subtract resource
+api.add_resource(
+    Subtract,
+    "/subtract"
+)
+
+# Use api to expose Multiply resource
+api.add_resource(
+    Multiply,
+    "/multiply"
+)
+
+# Use api to expose Divide resource
+api.add_resource(
+    Divide,
+    "/divide"
 )
 
 if __name__=="__main__":
